0bd6254c6d7e533b75741688e13c129afe536ace
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-10-14 16:23:24 -0400

    Add code to download data

---
+++ cdprep/dwnld_data/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © Geolearn
+# https://github.com/GeolearnAI/gwire
+#
+# This file is part of Gwire.
+# Licensed under the terms and conditions defined in file 'LICENSE.txt',
+# which is distributed along with this program.
+# -----------------------------------------------------------------------------
