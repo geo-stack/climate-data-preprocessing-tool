7af776d77f13dd22b2d9f3262ae64af2f015300a
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-10-14 10:43:47 -0400

    Add gui config file

---
+++ cdprep/config/gui.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © Climate Data Preprocessing Tool Project Contributors
+# https://github.com/cgq-qgc/climate-data-preprocessing-tool
+#
+# This file is part of Climate Data Preprocessing Tool.
+# Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
+
+ICON_COLOR = '#202020'
+GREEN = '#00aa00'
+RED = '#CC0000'
+YELLOW = '#ffa500'
+YELLOWLIGHT = '#fcf7b6'
+
+INIT_MAINWINDOW_SIZE = (1260, 740)
+FRAME_SYLE = 22
