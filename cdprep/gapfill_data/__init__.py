7d0f2da810a9f4b1ff81fb6e13c97cdabdd88ae7
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-10-20 14:36:01 -0400

    Create a gapfill_data module

---
+++ cdprep/gapfill_data/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © Climate Data Preprocessing Tool Project Contributors
+# https://github.com/cgq-qgc/climate-data-preprocessing-tool
+#
+# This file is part of Climate Data Preprocessing Tool.
+# Licensed under the terms of the GNU General Public License.
+# -----------------------------------------------------------------------------
