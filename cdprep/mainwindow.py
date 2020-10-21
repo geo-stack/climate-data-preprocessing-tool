2d0fe3d4b1935898acce4f11274eac398be01b1e
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2020-10-21 14:30:56 -0400

    Setup main window settings

--- cdprep/mainwindow.py
+++ cdprep/mainwindow.py
@@ -25,7 +25,8 @@ from cdprep.config.main import CONF
 from cdprep.config.icons import get_icon, get_iconsize
 from cdprep.dwnld_data.dwnld_data_manager import WeatherStationDownloader
 from cdprep.gapfill_data.gapfill_weather_gui import WeatherDataGapfiller
-from cdprep.utils.qthelpers import create_toolbar_stretcher
+from cdprep.utils.qthelpers import (
+    create_toolbar_stretcher, qbytearray_to_hexstate, hexstate_to_qbytearray)
 from cdprep.config.ospath import (
     get_select_file_dialog_dir, set_select_file_dialog_dir)
 
@@ -62,6 +63,8 @@ class MainWindow(QMainWindow):
         toolbar.addWidget(create_toolbar_stretcher())
         toolbar.addWidget(self._create_workdir_manager())
 
+        self._restore_window_geometry()
+        self._restore_window_state()
         self.set_workdir(CONF.get('main', 'working_dir', get_home_dir()))
 
     def _create_workdir_manager(self):
@@ -103,6 +106,53 @@ class MainWindow(QMainWindow):
         if dirname:
             set_select_file_dialog_dir(dirname)
             self.set_workdir(dirname)
+
+    # ---- Main window settings
+    def _restore_window_geometry(self):
+        """
+        Restore the geometry of this mainwindow from the value saved
+        in the config.
+        """
+        hexstate = CONF.get('main', 'window/geometry', None)
+        if hexstate:
+            hexstate = hexstate_to_qbytearray(hexstate)
+            self.restoreGeometry(hexstate)
+        else:
+            from gwhat.config.gui import INIT_MAINWINDOW_SIZE
+            self.resize(*INIT_MAINWINDOW_SIZE)
+
+    def _save_window_geometry(self):
+        """
+        Save the geometry of this mainwindow to the config.
+        """
+        hexstate = qbytearray_to_hexstate(self.saveGeometry())
+        CONF.set('main', 'window/geometry', hexstate)
+
+    def _restore_window_state(self):
+        """
+        Restore the state of this mainwindow’s toolbars and dockwidgets from
+        the value saved in the config.
+        """
+        # Then we appply saved configuration if it exists.
+        hexstate = CONF.get('main', 'window/state', None)
+        if hexstate:
+            hexstate = hexstate_to_qbytearray(hexstate)
+            self.restoreState(hexstate)
+
+    def _save_window_state(self):
+        """
+        Save the state of this mainwindow’s toolbars and dockwidgets to
+        the config.
+        """
+        hexstate = qbytearray_to_hexstate(self.saveState())
+        CONF.set('main', 'window/state', hexstate)
+
+    # ---- Qt method override/extension
+    def closeEvent(self, event):
+        """Qt method override to close the project before close the app."""
+        self._save_window_geometry()
+        self._save_window_state()
+        event.accept()
 
 
 if __name__ == '__main__':
